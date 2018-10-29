65e514410c9a851d313280fa77a3ff8fdc1e8f24
James Lee <jameslee1187@gmail.com>
2018-10-29 16:36:43 -0400

    Done.

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -7,13 +7,14 @@ describe "GET '/' - Greeting Form" do
     expect(page.body).to include("Welcome!")
   end
 
+
   # New test
   it 'has a greeting form with a user_name field' do
     visit '/'
 
     expect(page).to have_selector("form")
     expect(page).to have_field(:user_name)
-  end
+end
 end
 
 describe "POST '/greet' - User Greeting" do
